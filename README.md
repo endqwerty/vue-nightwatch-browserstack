57567f4880b69986d1959e175b5fc0c6fe6967b4
daposy <endqwerty@gmail.com>
2019-02-27 13:05:32 -0800

    updates

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 
 ## Installation
 
+**this depends on @vue/cli-plugin-e2e-nightwatch**
+
 In your existing Vue cli application, run the following command:
-`vue add nightwatch-browserstack`
+`npm install -D https://github.com:ijettech-dev/vue-nightwatch-browserstack`
 
 This will install a `/browserstack_config` folder with a `nightwatch.conf.js` file, a default test at `/tests/browserstack/default_test.test.js`, and adds several scripts to the `package.json` to run the tests in different browsers.
+
 
 Test results are also available at http://automate.browserstack.com
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = (api) => {
   api.registerCommand(
-    'test:browserstack',
-    {
+    'test:browserstack', {
       description: 'Run automated tests through BrowserStack',
       usage: 'vue-cli-service test:browesrstack [options]',
       options: {},
@@ -9,43 +8,46 @@ module.exports = (api) => {
     },
     (args) => {
       try {
-        const serverPromise = args.url ? Promise.resolve({ url: args.url }) : api.service.run('serve');
-        return serverPromise.then(({ server, url }) => {
+        const serverPromise = args.url ? Promise.resolve({
+          url: args.url
+        }) : api.service.run('serve');
+        return serverPromise.then(({
+          server,
+          url
+        }) => {
           process.env.VUE_DEV_SERVER_URL = url;
-                
+
           const nightwatch = require('nightwatch');
           const browserstack = require('browserstack-local');
 
           nightwatch.bs_local = bs_local = new browserstack.Local();
           bs_local.start({
-            'key': process.env.BROWSERSTACK_ACCESS_KEY,
-          }, 
-          (error) => {
-            if (error) {
-              throw error;
-            }
-            
-            console.log('Connected. Now testing...');
-            return nightwatch.cli((argv) => {
-              return nightwatch.CliRunner(argv)
-                .setup(null, () => {
-                  // Code to stop browserstack local after end of parallel test
-                  bs_local.stop(function(){});
-                  server.close();
-                })
-                .runTests(() => {
-                  // Code to stop browserstack local after end of single test
-                  bs_local.stop(function(){});
-                  server.close();
-                }
-              );
-            })
-          });
-          
-          
+              'key': process.env.BROWSERSTACK_ACCESS_KEY,
+            },
+            (error) => {
+              if (error) {
+                throw error;
+              }
+
+              console.log('Connected. Now testing...');
+              return nightwatch.cli((argv) => {
+                return nightwatch.CliRunner(argv)
+                  .setup(null, () => {
+                    // Code to stop browserstack local after end of parallel test
+                    bs_local.stop(function () {});
+                    server ? server.close() : console.log('no server');
+                  })
+                  .runTests(() => {
+                    // Code to stop browserstack local after end of single test
+                    bs_local.stop(function () {});
+                    server ? server.close() : console.log('no server');
+                  });
+              })
+            });
+
+
         });
-      }
-      catch (error) {
+      } catch (error) {
         console.log('There was an error while starting the test runner:\n\n');
         process.stderr.write(error.stack + '\n');
         process.exit(2);
