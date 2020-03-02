76158c582849ac1e1616a0b7bb5a3e3a5a3b14f4
Daniel Yang <endqwerty@gmail.com>
2020-03-02 15:09:28 -0800

    linting

--- generator/index.js
+++ generator/index.js
@@ -10,7 +10,7 @@ module.exports = (api, opts, rootOptions) => {
       'test:browserstack:chrome':
         'vue-cli-service test:browserstack -c browserstack_config/nightwatch.conf.js -e chrome -t "tests/e2e/specs/test.js"',
       'test:browserstack:firefox':
-        'vue-cli-service test:browserstack -c browserstack_config/nightwatch.conf.js -e firefox tests/e2e/specs/test.js'
+        'vue-cli-service test:browserstack -c browserstack_config/nightwatch.conf.js -e firefox tests/e2e/specs/test.js',
     },
   })
 }
